e6bd5ff25a2c39a44c515134c8e57eaf35b7df7b
Abdessalam Mounach <mounach-@hotmail.com>
2022-12-28 12:44:32 +0100

    chore: implement unset_cmd

--- unset.c
+++ unset.c
@@ -6,24 +6,36 @@
 /*   By: amounach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 20:11:44 by amounach          #+#    #+#             */
-/*   Updated: 2022/12/26 20:11:56 by amounach         ###   ########.fr       */
+/*   Updated: 2022/12/28 12:27:47 by amounach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void del_first_node(t_env **head_ref, char *key)
+void match_variables(t_env **env, char **str)
 {
-  t_env *to_delete;
+  int i;
 
-  to_delete = *head_ref;
-  if (!*head_ref)
-    return;
-  if (strcmp(key, to_delete->v_name) == 0)
+  t_env *tmp = *env;
+  i = 1;
+  while (str[i])
   {
-    to_delete = *head_ref;
-    head_ref = to_delete->next;
-    head_ref = to_delete->next;
-    free(to_delete);
+    tmp = *env;
+    while (tmp)
+    {
+      if (strcmp(str[i], tmp->v_name) == 0)// TODO: use ft_strcmp
+      {
+        // printf ("var name:%s\n",tmp->v_name);
+        printf ("string: %s\n", str[i]);
+        delete_onev(env, tmp);
+      }
+      tmp = tmp->next;
+    }
+    i++;
   }
+}
+
+void  unset(t_env **env, char **str)
+{
+    match_variables(env, str);
 }
