ba0f78aabae1e21ea932b4c5a218c5878b00d8f1
Abdessalam Mounach <mounach-@hotmail.com>
2023-01-04 00:11:16 +0100

    fix: Fix file existence check

--- heredoc.c
+++ heredoc.c
@@ -6,11 +6,12 @@
 /*   By: amounach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/31 00:37:32 by amounach          #+#    #+#             */
-/*   Updated: 2023/01/03 13:03:21 by amounach         ###   ########.fr       */
+/*   Updated: 2023/01/04 00:09:06 by amounach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
 static int	count_size(int nb)
 {
 	int	i;
@@ -55,69 +56,59 @@ char	*ft_itoa(int n)
 	return (str);
 }
 
-int file_is_exists(char *file_name)
+int	file_is_exists(const char *file_name)
 {
-  
-    struct stat buff;
-    
-    return (!stat(file_name, &buff));     
+	return (!access(file_name, F_OK));
 }
 
-char    *change_file_name(char *file_name, int *idx)
+char	*change_file_name(char *file_name, int *idx)
 {
-    
-    if (file_is_exists(ft_strjoin("/tmp/", file_name)))
-    {
-        return(change_file_name(ft_strjoin("heredoc", ft_itoa((*idx)++)), idx));
-        
-    }
-    return (ft_strjoin("/tmp/", file_name));
+	if (file_is_exists(ft_strjoin("/tmp/", file_name)))
+		return (change_file_name(ft_strjoin(file_name, ft_itoa((*idx)++)),
+				idx));
+	return (ft_strjoin("/tmp/", file_name));
 }
 
-void    check_eof(char **value, int  type)
+void	check_eof(char **value, int type)
 {
-    char    *line;
-    int     fd;
-    char    *buff;
-    int     idx;
-    
-    idx = 0;
-    if (type == VARIABLE)
-        *value = ft_strjoin("$", *value);
-    line = ft_strdup("");
-    buff = ft_strdup("");
-    while (1)
-    {
-        line = readline(">");
-        if (!line || !ft_strcmp(*value, line))
-            break;
-        else
-            buff = ft_strjoin(buff, ft_strjoin(line, "\n"));
-    } 
-    *value = change_file_name("heredoc", &idx);
-    puts(*value);
-    fd = open(*value, O_CREAT| O_RDWR| O_APPEND, 0644);
-    printf("%d\n", fd);
-    ft_putstr_fd(fd, buff);
-    close(fd);
+	char	*line;
+	int		fd;
+	char	*buff;
+	int		idx;
+
+	idx = 0;
+	if (type == VARIABLE)
+		*value = ft_strjoin("$", *value);
+	line = ft_strdup("");
+	buff = ft_strdup("");
+	while (1)
+	{
+		line = readline(">");
+		if (!line || !ft_strcmp(*value, line))
+			break ;
+		else
+			buff = ft_strjoin(buff, ft_strjoin(line, "\n"));
+	}
+	*value = change_file_name("heredoc", &idx);
+	fd = open(*value, O_CREAT | O_RDWR | O_TRUNC, 0644);
+	ft_putstr_fd(fd, buff);
+	close(fd);
 }
 
-void    open_heredoc(t_tokens *token)
+void	open_heredoc(t_tokens *token)
 {
-    t_tokens    *tmp;
+	t_tokens	*tmp;
 
-    tmp = token;
-    while (tmp)
-    {
-        if (tmp->type == HEREDOC)
-        {
-            if (tmp->next->type == WSPACE)
-            {   
-                check_eof(&tmp->next->next->value, tmp->next->next->type);
-            }
-            else
-                check_eof(&tmp->next->value, tmp->next->type);
-        }
-        tmp = tmp->next;
-    }
+	tmp = token;
+	while (tmp)
+	{
+		if (tmp->type == HEREDOC)
+		{
+			if (tmp->next->type == WSPACE)
+				check_eof(&tmp->next->next->value, tmp->next->next->type);
+			else
+				check_eof(&tmp->next->value, tmp->next->type);
+		}
+		tmp = tmp->next;
+	}
 }
