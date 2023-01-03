6d3e4d984a28e239e4701f99e4b617dbfbbbfc70
Abdessalam Mounach <mounach-@hotmail.com>
2023-01-04 00:19:52 +0100

    fix: Fix `HEREDOC` problems

--- redirections.c
+++ redirections.c
@@ -6,22 +6,22 @@
 /*   By: amounach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 17:44:56 by iel-bakk          #+#    #+#             */
-/*   Updated: 2023/01/03 14:30:20 by amounach         ###   ########.fr       */
+/*   Updated: 2023/01/04 00:01:48 by amounach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void launch_redir(t_redir *list)
+void	launch_redir(t_redir *list)
 {
-	int fd;
+	int	fd;
 
 	while (list)
 	{
 		if (list && list->type == RE_OUTPUT)
 		{
 			fd = open(list->file_name, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-			if(fd < 0)
+			if (fd < 0)
 			{
 				perror("");
 			}
@@ -32,7 +32,7 @@ void launch_redir(t_redir *list)
 		else if (list && list->type == APPEND)
 		{
 			fd = open(list->file_name, O_CREAT | O_WRONLY | O_APPEND, 0644);
-			if(fd < 0)
+			if (fd < 0)
 				perror("");
 			dup2(fd, 1);
 			list = list->next;
@@ -41,7 +41,7 @@ void launch_redir(t_redir *list)
 		else if (list && list->type == RE_INPUT)
 		{
 			fd = open(list->file_name, O_RDONLY, 0644);
-			if(fd < 0)
+			if (fd < 0)
 				perror("");
 			dup2(fd, 0);
 			list = list->next;
@@ -50,14 +50,15 @@ void launch_redir(t_redir *list)
 		else if (list && list->type == HEREDOC)
 		{
 			fd = open(list->file_name, O_RDONLY, 0644);
-			if(fd < 0)
+			if (fd < 0)
 				perror("");
 			dup2(fd, 0);
 			unlink(list->file_name);
 			list = list->next;
 			close(fd);
 		}
-		else if (list && (list->type != APPEND || list->type != RE_OUTPUT || list->type != RE_INPUT || list->type != HEREDOC))
+		else if (list && (list->type != APPEND || list->type != RE_OUTPUT
+				|| list->type != RE_INPUT || list->type != HEREDOC))
 			break ;
 	}
 }
