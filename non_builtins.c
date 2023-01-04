4f45b38448e3b5fea0682fe289ae257bb258c061
Abdessalam Mounach <mounach-@hotmail.com>
2023-01-04 20:01:56 +0100

    chore: add command not found exit status & fix norm

--- non_builtins.c
+++ non_builtins.c
@@ -3,31 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   non_builtins.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iel-bakk < iel-bakk@student.1337.ma>       +#+  +:+       +#+        */
+/*   By: amounach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 16:38:18 by iel-bakk          #+#    #+#             */
-/*   Updated: 2023/01/01 12:50:20 by iel-bakk         ###   ########.fr       */
+/*   Updated: 2023/01/04 15:43:45 by amounach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int compare_to_biultin(char *str)
+int	compare_to_biultin(char *str)
 {
-	if (ft_strcmp(str, "pwd") == 0 || ft_strcmp(str, "unset") == 0 || ft_strcmp(str, "export") == 0 || ft_strcmp(str, "cd") == 0 || ft_strcmp(str, "exit") == 0 || ft_strcmp(str, "echo") == 0 || ft_strcmp(str, "env") == 0)
+	if (ft_strcmp(str, "pwd") == 0 || ft_strcmp(str, "unset") == 0
+		|| ft_strcmp(str, "export") == 0 || ft_strcmp(str, "cd") == 0
+		|| ft_strcmp(str, "exit") == 0 || ft_strcmp(str, "echo") == 0
+		|| ft_strcmp(str, "env") == 0)
 		return (1);
 	return (0);
 }
 
-int check_builtin(char **s, t_env *env)
+int	check_builtin(char **s, t_env *env)
 {
-	char *str;
-	char *tmp;
+	char	*str;
+	char	*tmp;
 
 	str = *s;
 	if (!str)
 		return (-1);
-	if (str[0] == '/') // add a check with access syscall to deterin if that file exists and check before lauching execve if that file is executable.
+	if (str[0] == '/')
 	{
 		if (access((const char *)str, F_OK) != 0)
 			return (perror("command not found"), -1);
@@ -50,9 +53,9 @@ int check_builtin(char **s, t_env *env)
 	return (0);
 }
 
-int *creat_pipe(void)
+int	*creat_pipe(void)
 {
-	int *fd;
+	int	*fd;
 
 	fd = (int *)malloc(sizeof(int) * 2);
 	if (!fd)
@@ -62,14 +65,15 @@ int *creat_pipe(void)
 	return (fd);
 }
 
-int exec_cmd(t_final *t, char **env, int fd[2])
+int	exec_cmd(t_final *t, char **env, int fd[2])
 {
 	int pid;
 
 	pid = fork();
 	if (pid == 0)
 	{
-
+		signal(SIGQUIT, SIG_DFL);
+		signal(SIGINT, SIG_DFL);
 		if (fd[1] != -1)
 		{
 			dup2(fd[1], 1);
@@ -80,10 +84,11 @@ int exec_cmd(t_final *t, char **env, int fd[2])
 			dup2(fd[0], 0);
 			close(fd[0]);
 		}
-		launch_redir(t->list);
+		if (launch_redir(t->list))
+			exit(0);
 		execve(t->str[0], t->str, env);
 		write(2, "command not found\n", 18);
-		exit(0);
+		exit(127);
 	}
 	ft_free(env);
 	return (pid);
