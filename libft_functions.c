de11302ced08ecadc5b67b0b54ba2bd6a07b6e1d
Abdessalam Mounach <mounach-@hotmail.com>
2023-01-04 19:55:36 +0100

    chore: Add libft functions

---
+++ libft_functions.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft_functions.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amounach <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/04 15:02:08 by amounach          #+#    #+#             */
+/*   Updated: 2023/01/04 15:37:27 by amounach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *s1)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	if (!s1)
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * ft_strlen(s1) + 1);
+	if (!str)
+		return (NULL);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+int	ft_strlcpy(char *dst, char *src, int dstsize)
+{
+	int	r;
+	int	i;
+
+	i = 0;
+	r = 0;
+	while (src[i])
+		i++;
+	if (dstsize == 0)
+		return (i);
+	while (src[r] && r < dstsize - 1)
+	{
+		dst[r] = src[r];
+		r++;
+	}
+	dst[r] = '\0';
+	return (i);
+}
+
+static int	count_size(int nb)
+{
+	int	i;
+
+	i = 0;
+	if (nb <= 0)
+	{
+		nb *= -1;
+		i++;
+	}
+	while (nb != 0)
+	{
+		i++;
+		nb /= 10;
+	}
+	return (i);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	size_t	len;
+	long	nb;
+
+	nb = n;
+	len = count_size(n);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	if (nb < 0)
+		nb *= -1;
+	str[len] = '\0';
+	while (len--)
+	{
+		str[len] = (nb % 10) + 48;
+		nb /= 10;
+	}
+	if (n < 0)
+		str[0] = '-';
+	else if (n == 0)
+		str[0] = '0';
+	return (str);
+}
